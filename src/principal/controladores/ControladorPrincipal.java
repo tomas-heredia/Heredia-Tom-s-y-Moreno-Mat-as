aa256dfe8e8aaaf0f3a92d7c89c97182ac73c240
tomas <tomasheredia04@hotmail.com>
2020-10-15 11:29:30 -0300

    Agregado de los metodos de compartacion entre los elementos de cada clase, correcion de un nombre deu Alumno y separacion de cada conjunto de metodos.

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -25,6 +25,7 @@ import tipos.vistas.VentanaATipo;
 
 public class ControladorPrincipal {
     public static void main(String[] args) {
+        
         ArrayList<Alumno> deAlumno = new ArrayList<>();
         ArrayList<Profesor> deProfesor = new ArrayList<>();
         ArrayList<Idioma> deIdioma = new ArrayList<>();
@@ -40,25 +41,45 @@ public class ControladorPrincipal {
 
    
    
+//<editor-fold defaultstate="collapsed" desc="Alumnos">
     Alumno AlumnoUno = new Alumno(1234, "Heredia", "tomas" ,"si", "sx");
-    Alumno AlumnoDos = new Alumno(4321, "Meltroso", "Devora" ,"no", "cx");
-    Alumno AlumnoTres = new Alumno(42324, "Distancia", "Susana" ,"nel", "ds");    
+    Alumno AlumnoDos = new Alumno(4321, "Jolyne", "Cujoh" ,"no", "cx");
+    Alumno AlumnoTres = new Alumno(42324, "Distancia", "Susana" ,"nel", "ds");
     Alumno AlumnoCuatro = new Alumno(45454, "De La Fuente", "Ernesto" ,"pel", "fu");
     Alumno AlumnoCinco = new Alumno(666, "Dall'inferno ", "Dante" ,"yes", "fp");
 
     deAlumno.add (AlumnoUno);
-    deAlumno.add (AlumnoDos);
-    deAlumno.add (AlumnoTres);
-    deAlumno.add (AlumnoCuatro);
-    deAlumno.add (AlumnoCinco); 
+
+    if(!deAlumno.contains(AlumnoDos))
+    {
+        deAlumno.add (AlumnoDos);
+    }
+
+    if(!deAlumno.contains(AlumnoTres))
+    {
+        deAlumno.add (AlumnoTres);
+    }
+
+    if(!deAlumno.contains(AlumnoCuatro))
+    {
+        deAlumno.add (AlumnoCuatro);
+    }
+
+    if(!deAlumno.contains(AlumnoCinco))
+    {
+        deAlumno.add (AlumnoCinco);
+    }
+
+
 
     for (Alumno r : deAlumno)
-   {
-       r.mostrar();
-   }
-
+    {
+        r.mostrar();
+    }
+//</editor-fold>
     
     
+//<editor-fold defaultstate="collapsed" desc="Profesores">
     Profesor ProfesorUno = new Profesor(12334, "Del Sueldo", "Carolina", "sd", Cargo.TITULAR);
     Profesor ProfesorDos = new Profesor(999, "Hawking", "Stephen", "ed", Cargo.ADJUNTO);
     Profesor ProfesorTres = new Profesor(5464, "Dirac", "Paul", "jh", Cargo.ASOCIADO);
@@ -66,16 +87,36 @@ public class ControladorPrincipal {
     Profesor ProfesorCinco = new Profesor(0001, "Maron", "Virgilio", "kb", Cargo.JTP);
 
     deProfesor.add (ProfesorUno);
-    deProfesor.add (ProfesorDos);
-    deProfesor.add (ProfesorTres);
-    deProfesor.add (ProfesorCuatro);
-    deProfesor.add (ProfesorCinco);
+
+    if(!deProfesor.contains(ProfesorDos))
+    {
+        deProfesor.add (ProfesorDos);
+    }
+
+    if(!deProfesor.contains(ProfesorTres))
+    {
+        deProfesor.add (ProfesorTres);
+    }
+
+    if(!deProfesor.contains(ProfesorCuatro))
+    {
+        deProfesor.add (ProfesorCuatro);
+    }
+
+    if(!deProfesor.contains(ProfesorCinco))
+    {
+        deProfesor.add (ProfesorCinco);
+    }
+
 
     for (Profesor r : deProfesor)
-   {
-       r.mostrar();
-   }
-    
+    {
+        r.mostrar();
+    }
+//</editor-fold>
+
+
+//<editor-fold defaultstate="collapsed" desc="Idiomas">
     Idioma IdiomaUno = new Idioma("Español");
     Idioma IdiomaDos = new Idioma("Japones");
     Idioma IdiomaTres = new Idioma("Ingles");
@@ -83,17 +124,37 @@ public class ControladorPrincipal {
     Idioma IdiomaCinco = new Idioma("Elfico");
 
     deIdioma.add (IdiomaUno);
-    deIdioma.add (IdiomaDos);
-    deIdioma.add (IdiomaTres);
-    deIdioma.add (IdiomaCuatro);
-    deIdioma.add (IdiomaCinco);
 
-    for (Idioma r : deIdioma)  
-   {
-       System.out.println(r);
-   }
-   
+    if(!deIdioma.contains(IdiomaDos))
+    {
+        deIdioma.add (IdiomaDos);
+    }
+
+    if(!deIdioma.contains(IdiomaTres))
+    {
+        deIdioma.add (IdiomaTres);
+    }
+
+    if(!deIdioma.contains(IdiomaCuatro))
+    {
+        deIdioma.add (IdiomaCuatro);
+    }
+
+    if(!deIdioma.contains(IdiomaCinco))
+    {
+        deIdioma.add (IdiomaCinco);
+    }
+
+
+
+    for (Idioma r : deIdioma)
+    {
+        System.out.println(r);
+    }
+//</editor-fold>
+      
     
+//<editor-fold defaultstate="collapsed" desc="Lugares">
     Lugar LugarUno = new Lugar("Facultad");
     Lugar LugarDos = new Lugar("Congreso");
     Lugar LugarTres = new Lugar("Revista CIentífica");
@@ -101,65 +162,128 @@ public class ControladorPrincipal {
     Lugar LugarCinco = new Lugar("Ponencia");
 
     deLugar.add (LugarUno);
-    deLugar.add (LugarDos);
-    deLugar.add (LugarTres);
-    deLugar.add (LugarCuatro);
-    deLugar.add (LugarCinco);
+
+    if(!deLugar.contains(LugarDos))
+    {
+        deLugar.add (LugarDos);
+    }
+
+    if(!deLugar.contains(LugarTres))
+    {
+        deLugar.add (LugarTres);
+    }
+
+    if(!deLugar.contains(LugarCuatro))
+    {
+        deLugar.add (LugarCuatro);
+    }
+
+    if(!deLugar.contains(LugarCinco))
+    {
+        deLugar.add (LugarCinco);
+    }
+
 
     for (Lugar r : deLugar)
     {
-       System.out.println(r);     
+        System.out.println(r);
     }
+//</editor-fold>
     
     
-    PalabraClave palabraUno = new PalabraClave("www.League of Leagend.com");
-    PalabraClave palabraDos = new PalabraClave("www.Youtuve.com");
-    PalabraClave palabraTres = new PalabraClave("www.Spotify.com");
-    PalabraClave palabraCuatro = new PalabraClave("www.Twich.tv");  
-    PalabraClave palabraCinco = new PalabraClave("www.Facebook.com");  
-    PalabraClave palabraSeis = new PalabraClave("www.Genshin_Inpact.com");  
-    PalabraClave palabraSiete = new PalabraClave("www.meet.com");
-    
-     
-    dePalabraClave.add (palabraUno);
-    dePalabraClave1.add (palabraUno);
+//<editor-fold defaultstate="collapsed" desc="Palabras Claves">
+PalabraClave palabraUno = new PalabraClave("www.League of Leagend.com");
+PalabraClave palabraDos = new PalabraClave("www.Youtuve.com");
+PalabraClave palabraTres = new PalabraClave("www.Spotify.com");
+PalabraClave palabraCuatro = new PalabraClave("www.Twich.tv");
+PalabraClave palabraCinco = new PalabraClave("www.Facebook.com");
+PalabraClave palabraSeis = new PalabraClave("www.Genshin_Inpact.com");
+PalabraClave palabraSiete = new PalabraClave("www.meet.com");
+
+
+dePalabraClave.add (palabraUno);
+if(!dePalabraClave.contains(palabraDos))
+{
     dePalabraClave.add (palabraDos);
-    dePalabraClave2.add (palabraDos);
+}
+
+if(!dePalabraClave.contains(palabraTres))
+{
     dePalabraClave.add (palabraTres);
-    dePalabraClave1.add (palabraTres);   
+}
+
+if(!dePalabraClave.contains(palabraCuatro))
+{
     dePalabraClave.add (palabraCuatro);
-    dePalabraClave2.add (palabraCuatro);
+}
+
+if(!dePalabraClave.contains(palabraCinco))
+{
     dePalabraClave.add (palabraCinco);
+}
+
+if(!dePalabraClave.contains(palabraSeis))
+{
     dePalabraClave.add (palabraSeis);
-    dePalabraClave3.add (palabraSeis);
-    dePalabraClave4.add (palabraSeis);
+}
+
+if(!dePalabraClave.contains(palabraSiete))
+{
     dePalabraClave.add (palabraSiete);
-    dePalabraClave4.add (palabraSiete);
-     
-    for (PalabraClave r : dePalabraClave) 
-    {
-        System.out.println(r);   
-    }
+}
+dePalabraClave1.add (palabraUno);
+dePalabraClave2.add (palabraDos);
+dePalabraClave1.add (palabraTres);
+dePalabraClave2.add (palabraCuatro);
+dePalabraClave3.add (palabraSeis);
+dePalabraClave4.add (palabraSeis);
+dePalabraClave4.add (palabraSiete);
+
+for (PalabraClave r : dePalabraClave)
+{
+    System.out.println(r);
+}
+//</editor-fold>
     
     
+//<editor-fold defaultstate="collapsed" desc="Tipos">
     Tipo TipoUno = new Tipo ("trabajo en Congreso");
     Tipo TipoDos = new Tipo ("Capitulo de Libro");
     Tipo TipoTres = new Tipo ("Libro");
     Tipo TipoCuatro = new Tipo ("tesis");
     Tipo TipoCinco = new Tipo ("trabajo Recopilatorio");
-        
-    deTipo.add (TipoUno);
-    deTipo.add (TipoDos);
-    deTipo.add (TipoTres);
-    deTipo.add (TipoCuatro);
-    deTipo.add (TipoCinco);
 
-    for (Tipo r : deTipo) 
+
+    deTipo.add (TipoUno);
+
+    if(!deTipo.contains(TipoDos))
     {
-       System.out.println(r);     
+        deTipo.add (TipoDos);
     }
+
+    if(!deTipo.contains(TipoTres))
+    {
+        deTipo.add (TipoTres);
+    }
+
+    if(!deTipo.contains(TipoCuatro))
+    {
+        deTipo.add (TipoCuatro);
+    }
+
+    if(!deTipo.contains(TipoCinco))
+    {
+        deTipo.add (TipoCinco);
+    }
+
+    for (Tipo r : deTipo)
+    {
+        System.out.println(r);
+    }
+//</editor-fold>
     
      
+//<editor-fold defaultstate="collapsed" desc="Grupos">
     Grupo GrupoUno = new Grupo("Dinamita", "Grupo de Colegas");
     Grupo GrupoDos = new Grupo("LIga de la Justicia", "asociación de Heroes");
     Grupo GrupoTres = new Grupo("Liga de la Injusticia", "asociación de Villanos");
@@ -167,64 +291,102 @@ public class ControladorPrincipal {
     Grupo GrupoCinco = new Grupo("Los Externos", "Grupo de Deidades Externas");
 
     deGrupo.add (GrupoUno);
-    deGrupo.add (GrupoDos);
-    deGrupo.add (GrupoTres);
-    deGrupo.add (GrupoCuatro);
-    deGrupo.add (GrupoCinco);
 
-    for (Grupo r : deGrupo) 
+    if(!deGrupo.contains(GrupoDos))
     {
-       r.mostrar();
+        deGrupo.add (GrupoDos);
     }
+
+    if(!deGrupo.contains(GrupoTres))
+    {
+        deGrupo.add (GrupoTres);
+    }
+
+    if(!deGrupo.contains(GrupoCuatro))
+    {
+        deGrupo.add (GrupoCuatro);
+    }
+
+    if(!deGrupo.contains(GrupoCinco))
+    {
+        deGrupo.add (GrupoCinco);
+    }
+
+
+    for (Grupo r : deGrupo)
+    {
+        r.mostrar();
+    }
+//</editor-fold>
     
     
+//<editor-fold defaultstate="collapsed" desc="Miembros de grupo y Publicaciones">
     MiembroEnGrupo miembro1 = new MiembroEnGrupo(GrupoUno,  ProfesorUno, Rol.ADMINISTRADOR);
-    
-    LocalDate fecha = LocalDate.of(2020, 9, 24);        
-    
+
+    LocalDate fecha = LocalDate.of(2020, 9, 24);
+
     Publicacion publicacion1 = new Publicacion("Super Libro", fecha, "Link", "Super",dePalabraClave4, LugarUno, IdiomaUno, TipoUno, miembro1);
-      //   PUBLICACION 1   
+    //   PUBLICACION 1
 
 
     MiembroEnGrupo miembro2 = new MiembroEnGrupo(GrupoDos,  ProfesorUno, Rol.ADMINISTRADOR);
-   
+
     Publicacion publicacion2 = new Publicacion("Epopella", LocalDate.of(2020, 04, 20), "Link", "Historia", dePalabraClave3, LugarDos, IdiomaDos, TipoDos, miembro2);
     //   PUBLICACION 2
 
     MiembroEnGrupo miembro3 = new MiembroEnGrupo(GrupoTres,  ProfesorTres, Rol.COLAVORADOR);
 
     Publicacion publicacion3 = new Publicacion("Fisica 101", LocalDate.of(2020, 05, 17), "Link", "Ciencia", dePalabraClave2, LugarTres, IdiomaTres, TipoTres, miembro3);
-      //   PUBLICACION 3
-    
+    //   PUBLICACION 3
+
     MiembroEnGrupo miembro4 = new MiembroEnGrupo(GrupoCuatro,  ProfesorCuatro, Rol.COLAVORADOR);
 
     Publicacion publicacion4 = new Publicacion("Programacion para Tarados", LocalDate.of(2020, 07, 15), "Link", "Libro",dePalabraClave1, LugarCuatro, IdiomaCuatro, TipoCuatro, miembro4);
-      //   PUBLICACION 4
-   
+    //   PUBLICACION 4
+
     MiembroEnGrupo miembro5 = new MiembroEnGrupo(GrupoCinco,  ProfesorCinco, Rol.COLAVORADOR);
- 
+
     Publicacion publicacion5 = new Publicacion("La Divina Comedia", LocalDate.of(2020, 01, 01), "Link", "Hobra maestra", dePalabraClave, LugarCinco, IdiomaCinco, TipoCinco, miembro5);
-      //   PUBLICACION 5  
-    
-   
+    //   PUBLICACION 5
+
+
     dePublicacion.add(publicacion1);
-    dePublicacion.add(publicacion2);
-    dePublicacion.add(publicacion3);
-    dePublicacion.add(publicacion4);
-    dePublicacion.add(publicacion5);
-   
-    for (Publicacion r : dePublicacion) 
+    
+    if(!dePublicacion.contains(publicacion2))
+    {
+        dePublicacion.add(publicacion2);
+    }
+    
+     if(!dePublicacion.contains(publicacion3))
+    {
+        dePublicacion.add(publicacion3);
+    }
+    
+    if(!dePublicacion.contains(publicacion4))
+    {
+        dePublicacion.add(publicacion4);
+    }
+     
+    if(!dePublicacion.contains(publicacion5))
+    {
+        dePublicacion.add(publicacion5);
+    }
+    
+
+    for (Publicacion r : dePublicacion)
     {
         r.mostrar();
     }
+//</editor-fold>
     
    
     
   
     
     
-    //parte grafica
-    
+//<editor-fold defaultstate="collapsed" desc="Ventanas">
+//parte grafica
+
 ////        VentanaAMAlumno VentAlumno = new VentanaAMAlumno(null);
 ////        VentanaAMProfesor VentProfesor = new VentanaAMProfesor(null);
 ////        VentanaAMGrupo VentGrupo = new VentanaAMGrupo(null);
@@ -232,7 +394,7 @@ public class ControladorPrincipal {
 ////        VentanaALugar VentLugar = new VentanaALugar(null);
 ////        VentanaAPalabraClave VentPalabraclave = new VentanaAPalabraClave (null);
 ////        VentanaATipo VentTipo = new VentanaATipo(null);
-////        
+////
 ////        VentAlumno.setVisible(true);
 ////        VentProfesor.setVisible(true);
 ////        VentGrupo.setVisible(true);
@@ -240,6 +402,7 @@ public class ControladorPrincipal {
 ////        VentLugar.setVisible(true);
 ////        VentPalabraclave.setVisible(true);
 ////        VentTipo.setVisible(true);
+//</editor-fold>
        
     
     
